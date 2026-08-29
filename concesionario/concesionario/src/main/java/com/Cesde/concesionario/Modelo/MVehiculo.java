782d273ef3a2db067b9f20ab8e0cb9228ac3e71c
luis daniel perez pinedo <luisdaperezpinedo@gmail.com>
2026-08-29 00:28:38 +0000

    feat: modelo , entidad MVehiculo nombre = vehiculo atributos , encapsulamiento
    falto relacion entre tablas , sobrecarga de metodos

---
+++ concesionario/concesionario/src/main/java/com/Cesde/concesionario/Modelo/MVehiculo.java
@@ -0,0 +1,61 @@
+package com.Cesde.concesionario.Modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vehiculo")
+public class MVehiculo {
+    // Atributos
+    @Id
+    @Column(length = 6,nullable = false)
+    private String placa;
+    @Column(length = 25,nullable = false)
+    private String marca;
+    @Column(length = 4,nullable = false)
+    private String modelo;
+    @Column(nullable = false)
+    private Double valor;
+    @Column(nullable = false)
+    private Boolean activo;
+
+    // Relaciones entre tablas
+
+    // Encapsulamiento
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+}
